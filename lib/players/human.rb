1ca9e68dc66e2ed5156f27c2df1ae693d2df0edb
Samuel Cornet <samuel@noukod.com>
2018-10-23 13:45:22 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,16 @@
 
-class Player::Human < Player
+class Players::Human < Player
   def move(board, timer = 0)
     puts "Please enter 1-9:"
     gets.chomp
   end
 end
+
+# module Players
+#   class Human < Player
+#     def move(board)
+#     	puts "Enter your move: "
+#     	gets.chomp
+#     end
+#   end
+# end
