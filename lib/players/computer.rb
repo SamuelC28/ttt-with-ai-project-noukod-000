3e82b27a291e5855ec37d1155391179d1554a746
Samuel Cornet <samuel@noukod.com>
2018-10-23 13:52:15 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 require "pry"
 
-class Player::Computer < Player
+class Player::Computer < Players
   attr_accessor :board
   WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -79,3 +79,31 @@ class Player::Computer < Player
     end
   end
 end
+
+
+# module Players
+  class Computer < Player
+    def move(board)
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[6] == " "
+        "7"
+      elsif board.cells[8] == " "
+        "9"
+      elsif board.cells[1] == " "
+        "2"
+      elsif board.cells[3] == " "
+        "4"
+      elsif board.cells[5] == " "
+        "6"
+      elsif board.cells[7] == " "
+        "8"
+      end
+    end
+  end
+end
+
